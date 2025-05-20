4fef5122c020ce64181c4e74087a93ff801b44ea
Scholar <schorlar2468@gmail.com>
2025-05-20 10:48:29 +0000

    Logo icon added

--- components/custom-logo.tsx
+++ components/custom-logo.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function CustomLogo() {
   return (
     <Link href="/" className="flex items-center">
-      <div className="flex items-center">
+      <div className="flex gap-1 items-center">
+        <Image
+          src=
+          "/logo.png"
+
+          alt="Logo"
+          width={25}
+          height={25}
+          className="object-cover rounded-md"
+        />
         <span className="text-2xl font-bold tracking-tighter">
-          <span className="text-primary font-sora">S</span>CHOLAR
+          <span className="text-primary font-sora">S</span>cholar
         </span>
       </div>
     </Link>
